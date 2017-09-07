1cd5ee4fe6d51e872cb87dfa991989bdc6f978b5
Cameron Pickle <cmpickle@gmail.com>
2017-09-07 14:00:00 +0530

    updated window placement to work even if some windows are not open

--- windowplacement.py
+++ windowplacement.py
@@ -1,35 +1,51 @@
 import pyautogui as auto
 
+print("moving chrome....")
 chrome = auto.getWindow('Google Chrome')
-chrome.restore()
-chrome.move(-1920, 0)
-chrome.maximize()
+if chrome != None:
+	chrome.restore()
+	chrome.move(-1920, 0)
+	chrome.maximize()
 
+print("moving HipChat....")
 hipchat = auto.getWindow('HipChat')
-hipchat.restore()
-hipchat.move(2280, 250)
+if hipchat != None:
+	hipchat.restore()
+	hipchat.move(2280, 250)
 
+print("moving Spotify....")
 spotify = auto.getWindow('Spotify')
-spotify.restore()
-spotify.move(2200, 300)
+if spotify != None:
+	spotify.restore()
+	spotify.move(2200, 300)
 
+print("moving Outlook....")
 outlook = auto.getWindow('Outlook')
-outlook.restore()
-outlook.move(1920, 0)
-outlook.maximize()
+if outlook != None:
+	outlook.restore()
+	outlook.move(1920, 0)
+	outlook.maximize()
 
+print("moving Skype for Business....")
 skypeforbusiness = auto.getWindow('Skype for Business')
-skypeforbusiness.restore()
-skypeforbusiness.move(2000, 300)
+if skypeforbusiness != None:
+	skypeforbusiness.restore()
+	skypeforbusiness.move(2000, 300)
 
+print("moving Fiddler....")
 fiddler = auto.getWindow('Telerik Fiddler Web Debugger')
-fiddler.restore()
-fiddler.move(2280, 250)
+if fiddler != None:
+	fiddler.restore()
+	fiddler.move(2280, 250)
 
+print("moving Skype....")
 skype = auto.getWindow('Skype')
-skype.restore()
-skype.move(2280, 250)
+if skype != None:
+	skype.restore()
+	skype.move(2280, 250)
 
+print("moving Slack....")
 slack = auto.getWindow('Slack')
-slack.restore()
-slack.move(2280, 250)
+if slack != None:
+	slack.restore()
+	slack.move(2280, 250)
